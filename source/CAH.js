d8a5ff5589897b0c4ae0697c9a270438ec86d0ac
Tom Webster <samurailink3@gmail.com>
2012-11-28 05:14:35 -0500

    Joshuha Rebuild
    Project work from Joshuha's rebuild

--- source/CAH.js
+++ source/CAH.js
@@ -20,7 +20,38 @@ gapi.hangout.onApiReady.add(function (eventObj) {
 
         //update participants
         sendEvent('updateParticipantsList');
+        defaultFeed = gapi.hangout.layout.getDefaultVideoFeed();
+        videoCanvas = gapi.hangout.layout.getVideoCanvas();
+        //video canvas for reader
+        readerVideoWindow = Ext.create('Ext.window.Window', {
+            title:'Video',
+            id:'readerVideoWindow',
+            width:300,
+            height:200,
+            autoShow: true,
+            closable: false,
+            collapsible: false,
+            resizable: false,
+            shadow: false,
+            listeners : {
+                'move' : function(win,x,y,opt){
+                    videoCanvas.setPosition(x+5,y+28);
+                    videoCanvas.setVisible(true);
+                }
+            }
+        });
+        $('#readerVideoWindow').mousedown(function () {videoCanvas.setVisible(false);});
+        resetVideoWindow();
 
+        var pancakes = gapi.hangout.av.effects.createImageResource('http://tabletopforge.com/CAH/img/winner_pancakes.png');
+        pancakesOverlay = pancakes.createFaceTrackingOverlay({
+            'trackingFeature': gapi.hangout.av.effects.FaceTrackingFeature.NOSE_ROOT,
+            'scaleWithFace': true,
+            'rotateWithFace': true,
+            'scale': 1.0});
+
+        winnerSound = gapi.hangout.av.effects.createAudioResource(
+            'http://tabletopforge.com/CAH/img/Winner.wav').createSound({loop: false, localOnly: false});
     }
 });
 
@@ -34,10 +65,16 @@ function updateParticipantsList() {
                 id: participantArray[i].person.id,
                 name: participantArray[i].person.displayName,
                 imageURL: participantArray[i].person.image.url,
-                points: 0
+                points: 0,
+                displayIndex: 0 || participantArray[i].displayIndex,
+                participantID: participantArray[i].id
             });
         }
     }
+    Ext.getCmp('playerGrid').store.sort([
+        { property: 'displayIndex',  direction: 'ASC' },
+        { property: 'name', direction: 'ASC' }
+    ]);
 }
 
 //Helper functions
@@ -45,5 +82,42 @@ function uniqid()
 {
     unique_id++;
     return user.id+unique_id;
+}
+
+function resetVideoWindow() {
+    readerVideoWindow.alignTo(Ext.getBody(), "tr-tr", [-10, 10]);
+    videoCanvas.setWidth(readerVideoWindow.getWidth()-19);
+    videoCanvas.setVideoFeed(defaultFeed);
+    var pos = readerVideoWindow.getPosition();
+    videoCanvas.setPosition(pos[0]+5, pos[1]+28);
+    videoCanvas.setVisible(true);
+
+}
+
+function createTextOverlay(string) {
+    // Create a canvas to draw on
+    var canvas = document.createElement('canvas');
+    canvas.setAttribute('width', 166);
+    canvas.setAttribute('height', 100);
+
+    var context = canvas.getContext('2d');
+
+    // Draw background
+    context.fillStyle = '#BBB';
+    context.fillRect(0,0,166,50);
+
+    // Draw text
+    context.font = '32pt Impact';
+    context.lineWidth = 6;
+    context.lineStyle = '#000';
+    context.fillStyle = '#FFF';
+    context.fillColor = '#ffff00';
+    context.fillColor = '#ffff00';
+    context.textAlign = 'center';
+    context.textBaseline = 'bottom';
+    context.strokeText(string, canvas.width / 2, canvas.height / 2);
+    context.fillText(string, canvas.width / 2, canvas.height / 2);
+
+    return canvas.toDataURL();
 }
 
