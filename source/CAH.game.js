06969223054c2f11ff2c61709764d095265e7ef0
Tom Webster <samurailink3@gmail.com>
2013-03-24 14:02:15 -0400

    Added CAH Third Expansion Pack

--- source/CAH.game.js
+++ source/CAH.game.js
@@ -41,6 +41,7 @@ function startGame() {
                         { boxLabel: 'Base', name: 'sets', inputValue: 'Base', checked: true, readOnly:true },
                         { boxLabel: 'First Expansion', name: 'sets', checked: true, inputValue: 'CAHe1'},
                         { boxLabel: 'Second Expansion', name: 'sets', checked: true, inputValue: 'CAHe2' },
+                        { boxLabel: 'Third Expansion', name: 'sets', checked: true, inputValue: 'CAHe3' },
                         { boxLabel: 'Christmas Set', name: 'sets', checked: false, inputValue: 'CAHxmas' },
                         { boxLabel: 'Grognards (fan RPG set)', name: 'sets', checked: false, inputValue: 'CAHgrognards' },
                         { boxLabel: 'Weeaboo (fan Anime set)', name: 'sets', checked: false, inputValue: 'CAHweeaboo' },
