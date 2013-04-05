a2e88f7660917355b2bf3ecc3ca13de4a5bd3903
Joshuha Owen <joshuha@gmail.com>
2013-04-05 15:17:17 -0500

    Switched URLs to HangoutsAgainstHumanity.net

--- source/HAH.js
+++ source/HAH.js
@@ -1,6 +1,8 @@
 
 gapi.hangout.onApiReady.add(function (eventObj) {
     if (eventObj.isApiReady) {
-
+        angular.element(document).ready(function() {
+            angular.bootstrap(document);
+        });
     }
 });
