fc9eacf745d763c83f4a4e455d200809d0024c2f
Joshuha Owen <joshuha@gmail.com>
2013-04-14 09:35:27 -0500

    Angular bootstrap working Hello World with G+ profile info

--- source/HAH.hangout.js
+++ source/HAH.hangout.js
@@ -1,13 +1,11 @@
-function initGoogleAPI()
-{
-    //setup Google info
-    user = {"name": gapi.hangout.getParticipantById(participantID).person.displayName,
-            "id": gapi.hangout.getParticipantById(participantID).person.id,
-            "imageURL": gapi.hangout.getParticipantById(participantID).person.image.url
-    };
 
-    gapi.hangout.data.onStateChanged.add(onStateChanged);
+function UserCtrl($scope) {
+    var participantID = gapi.hangout.getParticipantId();
+    $scope.name = gapi.hangout.getParticipantById(participantID).person.displayName;
+    $scope.id = gapi.hangout.getParticipantById(participantID).person.id;
+    $scope.imageURL = gapi.hangout.getParticipantById(participantID).person.image.url;
 }
+
 
 //send event name and string version of JSON object to shared state
 function sendEvent(eventName, eventData) {
@@ -23,20 +21,22 @@ function sendEvent(eventName, eventData) {
 //Process received state change
 function onStateChanged(event) {
     try {
-        Ext.iterate(event.addedKeys, function (obj,index) {
-            //event function call
-            if (obj.key.search("##")> 0) {
-                var eventData = JSON.parse(obj.value);
-                var id = obj.key.search("##");
-                var funcName = obj.key.substr(0,id);
+        //get new key pairs
+        var newKeys = event.addedKeys;
+        //loop through keys
+        for(var i=0, len=newKeys.length; i < len; i++){
+            //event function call (use ## to identify event name)
+            if (newKeys[i].key.search("##")> 0) {
+                var eventData = JSON.parse(newKeys[i].value);
+                var id = newKeys[i].key.search("##");
+                var funcName = newKeys[i].key.substr(0,id);
                 eval(funcName + "(eventData)");
-                gapi.hangout.data.clearValue(obj.key);
+                gapi.hangout.data.clearValue(newKeys[i].key);
             }
-        });
-
+        }
     }
     catch (e) {
-
+        //log error
         console.log('Fail state changed');
         console.log(e);
         console.log(event);
--- source/HAH.js
+++ source/HAH.js
@@ -1,8 +1,10 @@
-
+//After Hangout API is ready
 gapi.hangout.onApiReady.add(function (eventObj) {
     if (eventObj.isApiReady) {
-        angular.element(document).ready(function() {
-            angular.bootstrap(document);
-        });
+        //manually bootstrap angular
+        angular.bootstrap(document.body);
+
+        //start listening to events
+        gapi.hangout.data.onStateChanged.add(onStateChanged);
     }
 });
